00e828f896a3d9c03ef44c4632cbc5d85ce3d838
Gusti <gustirizki26@gmail.com>
2024-04-14 14:14:36 +0700

    changes

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,20 +1,5 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 function LoginPage() {
-  const router = useRouter();
-  const handleLogin = () => {
-    router.push("/product");
-  };
-  return (
-    <div>
-      <div>Login Page</div>
-      <button onClick={() => handleLogin()}>Login</button>
-      <p>
-        Belum punya akun? Register <Link href={"/auth/register"}>Di Sini</Link>
-      </p>
-    </div>
-  );
+  return <></>;
 }
 
 export default LoginPage;
