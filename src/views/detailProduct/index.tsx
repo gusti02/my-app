9c6398f4d4278b80128690409806b6b7226c78e5
Gusti <gustirizki26@gmail.com>
2024-04-21 10:33:46 +0700

    change img to Image

--- src/views/detailProduct/index.tsx
+++ src/views/detailProduct/index.tsx
@@ -1,12 +1,17 @@
 import { ProductType } from "@/types/products.type";
-
+import Image from "next/image";
 function DetailProduct({ product }: { product: ProductType }) {
   return (
     <div className="flex justify-center items-center">
       <div className="w-1/4 p-5">
         <h1 className="text-center font-bold text-2xl">{product.name}</h1>
         <div className="">
-          <img src={product.image} alt={product.name} />
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={500}
+            height={500}
+          />
         </div>
         <h4 className="font-bold text-lg">{product.name}</h4>
         <p className="text-gray-500 text-lg">{product.category}</p>
